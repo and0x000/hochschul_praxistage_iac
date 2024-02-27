08280a668d21e58105af23ec5307a2c5073a0682
Andreas Schwarz <andreas.schwarz@hetzner.com>
2024-02-27 15:20:31 +0100

    basic server setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,44 @@
 terraform {
+  # https://developer.hashicorp.com/terraform/language/providers/requirements
   required_providers {
     hcloud = {
       source = "hetznercloud/hcloud"
     }
   }
 }
+
+# https://developer.hashicorp.com/terraform/language/values/variables
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+
+# https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+# https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs/resources/ssh_key
+resource "hcloud_ssh_key" "my_ssh_key" {
+  name       = "my_ssh_key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+# https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs/resources/server
+resource "hcloud_server" "my_server" {
+  name        = "my_server_1"
+  image       = "docker-ce"
+  location    = "nbg1"
+  ssh_keys    = [ "my_ssh_key" ]
+  server_type = "cx11"
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+
+  depends_on = [
+    hcloud_ssh_key.my_ssh_key
+  ]
+}
+
